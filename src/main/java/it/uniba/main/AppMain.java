c396129ea575431cc2dc8143ea21309c69323f47
Christian Riefolo <c.riefolo2@studenti.uniba.it>
2021-04-12 19:27:09 +0200

    Provata la stampa della damiera con numerazione

--- src/main/java/it/uniba/main/AppMain.java
+++ src/main/java/it/uniba/main/AppMain.java
@@ -7,73 +7,97 @@ import java.security.GeneralSecurityException;
 
 // import it.uniba.sotorrent.GoogleDocsUtils;
 // disattivato per problemi di compilazione
-
 /**
  * The main class for the project. It must be customized to meet the project
  * assignment specifications.
- * 
+ *
  * <b>DO NOT RENAME</b>
  */
 public final class AppMain {
 
-	/**
-	 * Private constructor. Change if needed.
-	 */
-	private AppMain() {
+    /**
+     * Private constructor. Change if needed.
+     */
+    private AppMain() {
 
-	}
+    }
 
-	/**
-	 * 	 * This is the main entry of the application.
-	 *
-	 * @param args The command-line arguments.
-	 */
-	public static void main(final String[] args) {
+    /**
+     * 	 * This is the main entry of the application.
+     *
+     * @param args The command-line arguments.
+     */
+    public static void main(final String[] args) {
 
-		System.out.println("Current working dir: " + System.getProperty("user.dir"));
+        System.out.println("Current working dir: " + System.getProperty("user.dir"));
 
-		if (args.length > 0) {
-			switch (args[0]) {
-			case "it":
-				System.out.println("Applicazione avviata.");
-				break;
+        if (args.length > 0) {
+            switch (args[0]) {
+                case "it":
+                    System.out.println("Applicazione avviata.");
+                    break;
 
-			case "en":
-				System.out.println("Application started.");
-				break;
+                case "en":
+                    System.out.println("Application started.");
+                    break;
 
-			default:
-				System.out.println("Specify the language. "
-								   + "Languages supported: 'it' or 'en'");
-				break;
-			}
-		} else {
-			System.out.println("Using default language 'en'");
-			System.out.println("Application started.");
-		}
+                default:
+                    System.out.println("Specify the language. "
+                            + "Languages supported: 'it' or 'en'");
+                    break;
+            }
+        } else {
+            System.out.println("Using default language 'en'");
+            System.out.println("Application started.");
+        }
 
-                int i = 0;
-                boolean isTrue = false;
+        int i = 0;
+        boolean isTrue = false;
 
-                while (i < args.length && !isTrue) {
-                    if (args[i].equals("--help") || args[i].equals("-h")) {
-                        Help();
-                        isTrue = true;
-                    }
-                    i++;
+        while (i < args.length && !isTrue) {
+            if (args[i].equals("--help") || args[i].equals("-h")) {
+                Help();
+                isTrue = true;
+            }
+            i++;
+        }
+        char damiera[][] = new char[8][];
+        int numCasella = 1;
+        int k;
+        int j;
+
+        for (k = 0; k < damiera.length; k++) {
+           
+            for (j = 0; j < damiera.length; j++) {
+                if((k + j) % 2 == 0){
+                    
+     System.out.print(numCasella);
+     
+                    //System.out.print(numCasella);
+                    numCasella++;
                 }
-	}
-        
-        public static void Help() {
-            System.out.println("\n | ---------- Dama Help Center ---------- | ");
-            System.out.println("Benvenuto in Dama, il gioco tradizionale per 2 giocatori");
-            System.out.println("Ecco un elenco dei comandi che puoi eseguire:"
+                else{
+                    System.out.print("-");
+                }
+                   
+            }
+           
+            System.out.print("\n");
+            
+        }
+    }
+
+    public static void Help() {
+        System.out.println("\n | ---------- Dama Help Center ---------- | ");
+        System.out.println("Benvenuto in Dama, il gioco tradizionale per 2 giocatori");
+        System.out.println("Ecco un elenco dei comandi che puoi eseguire:"
                 + "\n - Nuova Partita (gioca)"
                 + "\n - Abbandona (abbandona)"
                 + "\n - Esci (esci)"
                 + "\n - Mostrare la damiera con numerazione (numeri)"
                 + "\n - Mostrare la damiera con i pezzi (damiera)"
                 + "\n - Mostrare il tempo di gioco (tempo)");
-        }
-  
+
+    }
+
 }
