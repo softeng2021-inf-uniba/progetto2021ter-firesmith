e01d7d02c32bb699d350218d04c4909330a044fe
AntonioPapeo6 <antonio.papeo6@gmail.com>
2021-04-13 17:30:44 +0200

    Creata classe Parita

---
+++ src/main/java/it/uniba/main/Partita.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.uniba.main;
+
+/**
+ *
+ * @author anton
+ */
+public class Partita {
+    
+    public void Gioca(){
+
+    }
+    
+    public void MostraDamieraNum(){
+        
+    }
+    
+    public void MostraDamieraPz(){
+        
+    }
+    
+    public void MostraTempo(){
+        
+    }
+    
+    public void Abbandona(){
+        
+    }
+    
+    public void Esci(){
+        
+    }
+            
+}
