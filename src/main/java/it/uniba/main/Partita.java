2217cebfc4c5f1515a6390ba802a682f94663607
e-molinari <e.molinari3@studenti.uniba.it>
2021-04-15 12:13:34 +0200

    Aggiunta opzione damiera durante la partita

--- src/main/java/it/uniba/main/Partita.java
+++ src/main/java/it/uniba/main/Partita.java
@@ -7,6 +7,8 @@ package it.uniba.main;
 
 import java.util.*;
 
+import static it.uniba.main.AppMain.Help;
+
 /**
  *
  * @author anton
@@ -17,6 +19,7 @@ public class Partita {
 
     private String giocatore1 = "",giocatore2 = "" ;
     private boolean IsBlack = false, IsWhite = false;
+    Damiera d1 = new Damiera();
 
     public void Gioca(){
         if(partitaInCorso==true){
@@ -25,18 +28,66 @@ public class Partita {
         }
         else{
             partitaInCorso=true;
+
             System.out.println("Iniziando una nuova partita..." +
                                 "\nScegli il giocatore: " +
                                 "\n- Bianco" +
                                 "\n- Nero");
 
             ImpostaGiocatore();
-            System.out.println("Il giocatore 1 ha scelto il colore :" + giocatore1);
-                System.out.println("Il giocatore 2 ha scelto il colore " + giocatore2);
+            System.out.println("Il giocatore 1 ha scelto il colore: " + giocatore1);
+            System.out.println("Il giocatore 2 ha scelto il colore: " + giocatore2);
         }
+        ComandiPartita();
+    }
+
+    public void ComandiPartita() {
+        boolean PartitaInCorso = true;
+
+        do {
+            System.out.print("Scrivere un comando:"
+                    +"\n - --help | -h"
+                    + "\n - numeri"
+                    + "\n - damiera" +
+                    "\n> ");
+            Scanner in = new Scanner(System.in);
+            String comando = in.nextLine();
+
+            switch(comando){
+                case "--help":
+                    Help();
+                    break;
+
+                case "-h":
+                    Help();
+                    break;
+
+                case "numeri":
+                    d1.StampaNumeri();
+                    break;
+
+                case "damiera":
+                    d1.StampaPezzi();
+                    break;
+
+                case "gioca":
+                    System.out.println("La partita è già in corso!");
+                    break;
+
+                case "esci":
+                    // TODO DA IMPLEMENTARE @Giacomo
+                    PartitaInCorso = false;
+                    break;
+
+                default:
+                    System.out.println("Inserire un comando valido");
+                    break;
+            }
+        } while (PartitaInCorso == true);
     }
 
     public void ImpostaGiocatore() {
+        // TODO switch(comando)
         Scanner input = new Scanner(System.in);
         String Giocatore = input.nextLine();
         //...controlli per ortografia...
