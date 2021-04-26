fc3af85e0ffdb8c80d2d3f9380e548800245fad6
e-molinari <e.molinari3@studenti.uniba.it>
2021-04-26 18:23:53 +0200

    Aggiunto sistema con turni per giocatori

--- src/main/java/it/uniba/main/Partita.java
+++ src/main/java/it/uniba/main/Partita.java
@@ -9,17 +9,15 @@ import java.util.*;
 
 import static it.uniba.main.AppMain.*;
 
-/**
- *
- * @author anton
- */
 
 /*CLASSE PARTITA: classe che racchiude i metodi e gli attributi necessari per gestire una partita*/
 public class Partita {
 
     boolean partitaInCorso = false;
 
-    private String giocatore1 = "", giocatore2 = "";
+    Giocatore giocatore1 = new Giocatore();
+    Giocatore giocatore2 = new Giocatore();
+
     private boolean IsBlack = false, IsWhite = false;
 
     private long startTime = System.currentTimeMillis(); // Variabile in cui è memorizzato l'istante di tempo
@@ -41,13 +39,13 @@ public class Partita {
                     "\n➤ ");
 
             ImpostaGiocatore();
-            System.out.print("\nIl giocatore 1 ha scelto il colore: " + giocatore1 + " ");
+            System.out.print("\nIl giocatore 1 ha scelto il colore: " + giocatore1.getColore() + " ");
             if (IsWhite) {
                 System.out.println("⛂");  // Pedina bianca
             } else {
                 System.out.println("⛀");  // Pedina nera
             }
-            System.out.print("Il giocatore 2 ha scelto il colore: " + giocatore2 + " ");
+            System.out.print("Il giocatore 2 ha scelto il colore: " + giocatore2.getColore() + " ");
             if (IsWhite) {
                 System.out.println("⛀");  // Pedina nera
             } else {
@@ -55,24 +53,46 @@ public class Partita {
             }
             System.out.println();
         }
-        Menu.Partita();
+
+        int counter = 1;
+        do {
+            System.out.println("Turno " + counter);
+            if ((giocatore1.getColore()).equals("bianco")) {
+                System.out.println("Tocca al giocatore 1");
+                Menu.Partita();
+                if(partitaInCorso == true) {
+                    System.out.println("Tocca al giocatore 2");
+                    Menu.Partita();
+                }
+            } else {
+                System.out.println("Tocca al giocatore 2");
+                Menu.Partita();
+                if(partitaInCorso == true) {
+                    System.out.println("Tocca al giocatore 1");
+                    Menu.Partita();
+                }
+            }
+            counter++;
+        } while (partitaInCorso == false);
+
     }
 
     // Metodo che imposta il colore per entrambi i giocatori, sulla base della scelta del giocatore 1
     public void ImpostaGiocatore() {
+
         do {
             Scanner input = new Scanner(System.in);
             String Giocatore = input.nextLine();
             Giocatore = Giocatore.toLowerCase();
 
             if (Giocatore.equals("bianco")) {
-                IsWhite = true;
-                giocatore1 = Giocatore;
-                giocatore2 = "nero";
+                IsWhite = true;                 // serve solo per il while
+                giocatore1.setColore(Giocatore);
+                giocatore2.setColore("nero");
             } else if (Giocatore.equals("nero")) {
-                IsBlack = true;
-                giocatore1 = Giocatore;
-                giocatore2 = "bianco";
+                IsBlack = true;                 // serve solo per il while
+                giocatore1.setColore(Giocatore);
+                giocatore2.setColore("bianco");
             } else {
                 System.out.print("\n ⚠ Inserito comando sbagliato! Riprova." +
                         "\n➤ ");
@@ -86,13 +106,15 @@ public class Partita {
 
     // Metodo che mostra il tempo trascorso per il giocatore 1 (il primo che interagisce con il programma)
     public void MostraTempo(long startTime) {
+        String g1 = giocatore1.getColore(), g2 = giocatore2.getColore();
+
         long endTime = System.currentTimeMillis();
 
         long resultTime = (endTime - startTime) / 1000;
         if (resultTime < 60) {
-            System.out.println("\n \uD83D\uDD51 Il tempo trascorso dall'inizio della partita è: " + resultTime + " secondi (" + giocatore1 + ")\n");
+            System.out.println("\n \uD83D\uDD51 Il tempo trascorso dall'inizio della partita è: " + resultTime + " secondi (" + g1 + ")\n");
         } else {
-            System.out.println("\n \uD83D\uDD51 Il tempo trascorso dall'inizio della partita è: " + resultTime / 60 + " minuto/i (" + giocatore1 + ")" + "\n");
+            System.out.println("\n \uD83D\uDD51 Il tempo trascorso dall'inizio della partita è: " + resultTime / 60 + " minuto/i (" + g2 + ")" + "\n");
         }
     }
 }
