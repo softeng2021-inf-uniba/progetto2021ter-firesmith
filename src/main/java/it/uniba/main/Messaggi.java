fb8e94b07a08513bade35dd16d278b3073cf47b2
GiacomoSignorile <g.signorile14@studenti.uniba.it>
2021-04-27 16:28:58 +0200

    Inseriti comandi in classe Messaggi

--- src/main/java/it/uniba/main/Messaggi.java
+++ src/main/java/it/uniba/main/Messaggi.java
@@ -1,4 +1,28 @@
 package it.uniba.main;
 
 public class Messaggi {
+
+    // Metodo che mostra informazioni sui comandi disponibili
+    public static void Help() {
+        System.out.println("\n┌───────────────────────────┒"
+                +  "\n│     Dama Help Center      │"
+                +  "\n└───────────────────────────┘"
+                + "\n ⓘ Benvenuto in Dama, il gioco tradizionale per 2 giocatori\n"
+                + "\nEcco un elenco dei comandi che puoi eseguire:"
+                + "\n ♢ Nuova Partita (gioca)"
+                + "\n ♢ Abbandona (abbandona)"
+                + "\n ♢ Esci (esci)"
+                + "\n ♢ Mostrare la damiera con numerazione (numeri)"
+                + "\n ♢ Mostrare la damiera con i pezzi (damiera)"
+                + "\n ♢ Mostrare il tempo di gioco (tempo)" +
+                "\n");
+    }
+
+    public static void MsgUscita() {
+        System.out.println("\n \uD83D\uDEAA Uscita dal gioco...");
+    }
+
+    public static void MsgInserimentoSbagliato() {
+        System.out.println("\n ⚠ Comando non valido \n");
+    }
 }
