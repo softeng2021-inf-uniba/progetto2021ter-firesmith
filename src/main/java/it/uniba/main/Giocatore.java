7274415f7fed84fdbcfcbaefe54bca757b6b3181
GiacomoSignorile <g.signorile14@studenti.uniba.it>
2021-05-06 11:07:23 +0200

    Aggiunto setter and getter tempo per il giocatore

--- src/main/java/it/uniba/main/Giocatore.java
+++ src/main/java/it/uniba/main/Giocatore.java
@@ -16,10 +16,13 @@ public class Giocatore {
     private int PedineMangiate;
 
     public void setColore(String Colore) { this.Colore = Colore; }
-    public void setTempo(long Tempo) { this.Tempo = Tempo; }
+    public void setTempo(long Tempo) {
+    this.Tempo += Tempo ;
+    }
     public void setPedineMangiate(int valore) { PedineMangiate += valore; }
 
     public String getColore() { return Colore; }
+
     public long getTempo() { return Tempo; }
     public int getPedineMangiate() { return PedineMangiate; }
 }
