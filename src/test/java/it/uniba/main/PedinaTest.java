58a657da44e7fa2596fa47dd258d95b4817446fb
Giacomo Signorile <g.signorile14@studenti.uniba.it>
2021-05-21 19:45:21 +0200

    Aggiunti ulteriori test per la classe Pedina

--- src/test/java/it/uniba/main/PedinaTest.java
+++ src/test/java/it/uniba/main/PedinaTest.java
@@ -1,4 +1,18 @@
+package it.uniba.main;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PedinaTest {
-  
+    Pedina test = new Pedina(2,3);
+    @Test
+    void getRigaTest() {
+        assertEquals(2,test.getRiga());
+    }
+
+    @Test
+    void getColonnaTest() {
+        assertEquals(3,test.getColonna());
+    }
 }
