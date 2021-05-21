b9f5193806eb67601334b244b7ea67d4d67ccfcd
Giacomo Signorile <g.signorile14@studenti.uniba.it>
2021-05-21 19:43:24 +0200

    Aggiunti ulteriori test per la classe Damiera

--- src/test/java/it/uniba/main/DamieraTest.java
+++ src/test/java/it/uniba/main/DamieraTest.java
@@ -1,32 +1,42 @@
 package it.uniba.main;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Assumptions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.Ignore;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 import it.uniba.main.Damiera;
 
 
 public class DamieraTest {
+    Damiera test = new Damiera();
     @Test
     public void testDamiera(){
-        Damiera test = new Damiera();
         assertTrue(test.getDamieraPedine(4,4).getBlank());
         assertFalse(test.getDamieraPedine(0,0).getWhite());
         assertTrue(test.getDamieraPedine(7,7).getWhite());
     }
     @Test
     public void cercaColonnaTest() {
-            Damiera toTest = new Damiera();
-            toTest.cercaColonna(9);
-            assertEquals(0, toTest.cercaColonna(9));
+            test.cercaColonna(9);
+            assertEquals(0, test.cercaColonna(9));
     }
     @Test
     public void cercaRigaTest() {
-        Damiera toTest = new Damiera();
-        toTest.cercaRiga(9);
-        assertEquals(2, toTest.cercaRiga(9));
+        test.cercaRiga(9);
+        assertEquals(2, test.cercaRiga(9));
+    }
+    @Ignore
+    @Test
+    @DisplayName("disabled")
+    public void stampadamieranumeritest(){
+        test.stampaPosizioniPedine();
+
+    }
+    @Ignore
+    @Test
+    @DisplayName("disabled")
+    public void stampadamieratest(){
+        test.stampaDamieraPedine();
     }
 
 }
