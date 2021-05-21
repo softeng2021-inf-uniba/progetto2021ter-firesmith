68179ccf7d1606a74f4133122cd9b24c788d3b96
Giacomo Signorile <g.signorile14@studenti.uniba.it>
2021-05-21 19:44:57 +0200

    Aggiunti ulteriori test per la classe Partita

--- src/test/java/it/uniba/main/PartitaTest.java
+++ src/test/java/it/uniba/main/PartitaTest.java
@@ -1,6 +1,8 @@
 package it.uniba.main;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import it.uniba.main.*;
 
@@ -9,11 +11,37 @@ import java.time.Instant;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PartitaTest {
-
+    Partita test = new Partita();
     @Test
     void statoPartitaTest() {
-        Partita test = new Partita();
         test.setStato(true);
         assertTrue(test.getStato());
     }
+    @Test
+    void turnoPartitaTest() {
+        test.setTurno(true);
+        assertTrue(test.getTurno());
+    }
+    @Test
+    void setAbbandonaTest() {
+        test.setAbbandona(true);
+        assertTrue(test.getAbbandona());
+    }
+    @Ignore
+    @Test
+    @DisplayName("disabled")
+    void setTest() {
+        test.stampaPedineMangiate();
+        test.getCronologiaMosse();
+        test.getGiocatore1();
+    }
+    @Ignore
+    @Test
+    @DisplayName("disabled")
+    void setCronologiaMosseTest(){
+        test.setCronologiaMosse("12-13");
+        test.getCronologiaMosse();
+    }
+
+
 }
