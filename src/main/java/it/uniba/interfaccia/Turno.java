57f3fd3c9f0be4ab9be64b1ca4da764959bdab50
Giacomo Signorile <g.signorile14@studenti.uniba.it>
2021-05-26 17:22:08 +0200

    Creata interfaccia Turno

--- src/main/java/it/uniba/interfaccia/Turno.java
+++ src/main/java/it/uniba/interfaccia/Turno.java
@@ -1,2 +1,10 @@
-package it.uniba.interfaccia;public interface Turno {
+package it.uniba.interfaccia;
+
+import it.uniba.gioco.Partita;
+
+
+public interface Turno {
+
+        public void turnoGiocatore(Partita p);
+
 }
