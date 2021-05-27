9b38826b25f116b3c469a62c7ad8cc666a6f5034
Giacomo Signorile <g.signorile14@studenti.uniba.it>
2021-05-27 17:53:05 +0200

    Aggiunti casi di test per la gestione delle regex

--- src/test/java/it/uniba/strumenti/ComandoTest.java
+++ src/test/java/it/uniba/strumenti/ComandoTest.java
@@ -1,7 +1,29 @@
 package it.uniba.strumenti;
 
+import it.uniba.gioco.Mossa;
+import it.uniba.gioco.MossaBianco;
+
+import org.junit.jupiter.api.Test;
+
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ComandoTest {
-
+    Comando test = new Comando();
+    @Test
+    public void gestioneRegexSpostamentoTest(){
+        Mossa mossa = new MossaBianco(9,13);
+        assertEquals("spostamento",(test.gestisciRegex(mossa,"9-13")));
+    }
+    @Test
+    public void gestioneRegexPresaTest(){
+        Mossa mossa = new MossaBianco(9,13);
+        assertEquals("presa semplice",(test.gestisciRegex(mossa,"9x13")));
+    }
+    @Test
+    public void gestioneRegexPresaMultiplaTest(){
+        Mossa mossa = new MossaBianco(9,13);
+        assertEquals("presa multipla",(test.gestisciRegex(mossa,"9x13x21")));
+    }
 }
