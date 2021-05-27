f462aa5a45075fffd87f07c5378aad668b2c6ba2
Giacomo Signorile <g.signorile14@studenti.uniba.it>
2021-05-27 17:48:52 +0200

    Aggiunti casi di test per la classe Messaggi

---
+++ src/test/java/it/uniba/strumenti/MessaggiTest.java
@@ -0,0 +1,112 @@
+package it.uniba.strumenti;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessaggiTest {
+
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setStreams() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void restoreInitialStreams() {
+        System.setOut(originalOut);
+    }
+    @Test
+    public void uscitaTest(){
+        System.out.println(Messaggi.uscita());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void benvenutoTest(){
+        System.out.println(Messaggi.benvenuto());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void aiutoTest(){
+        System.out.println(Messaggi.aiuto());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void menuTest(){
+        System.out.println(Messaggi.menu());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void inserimentoTest(){
+        System.out.println(Messaggi.inserimento());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void partitaTest(){
+        System.out.println(Messaggi.partita());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void messdamieraTest(){
+        System.out.println(Messaggi.damiera());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public  void biancoAbbandonaTest(){
+        System.out.println(Messaggi.biancoAbbandona());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void neroAbbandonaTest(){
+        System.out.println(Messaggi.neroAbbandona());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void menuBiancoTest(){
+        System.out.println(Messaggi.menuBianco());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void menuNeroTest(){
+        System.out.println(Messaggi.menuNero());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void errorePartitaTest(){
+        System.out.println(Messaggi.errorePartita());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void presaTest(){
+        System.out.println(Messaggi.presa());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void spostamentoTest(){
+        System.out.println(Messaggi.spostamento());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void presaOkTest(){
+        System.out.println(Messaggi.presaOk());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void spostamentoOkTest(){
+        System.out.println(Messaggi.spostamentoOk());
+        assertNull(null, out.toString());
+    }
+    @Test
+    public void nonValidaTest(){
+        System.out.println(Messaggi.nonValida());
+        assertNull(null, out.toString());
+    }
+
+}
