5813ec7db03c1668a0078d8dd6938807916a5866
giaco <g.signorile14@studenti.uniba.it>
2021-05-28 19:51:19 +0200

    Aggiunto caso di test per il turno nero

--- src/test/java/it/uniba/interfaccia/TurnoNeroTest.java
+++ src/test/java/it/uniba/interfaccia/TurnoNeroTest.java
@@ -1,30 +1,41 @@
 package it.uniba.interfaccia;
 
+
 import it.uniba.gioco.Partita;
 import it.uniba.interfaccia.Menu;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MenuTest {
-    Menu test = new Menu();
+class TurnoNeroTest {
+    Turno test = new TurnoNero();
     private final ByteArrayOutputStream out = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
 
-    @Before
+    @BeforeEach
     public void setStreams() {
         System.setOut(new PrintStream(out));
     }
 
-    @After
+    @AfterEach
     public void restoreInitialStreams() {
         System.setOut(originalOut);
     }
 
+    @Test
+    public void turnoGiocBiancoSpostamentoTest() {
+        Partita partita = new Partita();
+        String input = "9-13";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        test.turnoGiocatore(partita,53);
+    }
 
 }
