50c3ce05c223c41dcea85ca8bd872c14ff08c1ce
t-perniola <t.perniola1@studenti.uniba.it>
2021-05-29 19:48:02 +0200

    Ri-aggiunti commenti javadoc in Turno.java

--- src/main/java/it/uniba/interfaccia/Turno.java
+++ src/main/java/it/uniba/interfaccia/Turno.java
@@ -2,9 +2,22 @@ package it.uniba.interfaccia;
 
 import it.uniba.gioco.Partita;
 
-
+/**
+ * <h1>Interfaccia che presenta un solo metodo TurnoGiocatore</h1><br>
+ * Questa interfaccia e' implementata dalle classi TurnoBianco e TurnoNero
+ * poiche' ognuna di esse offre una diversa implementazione per lo stesso
+ * metodo.
+ *
+ * @author Gruppo Firesmith
+ */
 public interface Turno {
 
+        /**
+         * Metodo implementato dalle classi TurnoBianco e TurnoNero
+         * @param p la partita in corso
+         * @param tempoG il tempo cumulativo del giocatore aggiornato ad ogni turno
+         * @return il tempo trascorso dal giocatore nel singolo turno
+         */
         public long turnoGiocatore(Partita p, long tempoG);
 
 }
