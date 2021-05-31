5051fa96f096b694396d583f5c9f0fbd5f94bd24
giaco <g.signorile14@studenti.uniba.it>
2021-05-31 11:03:24 +0200

    Aggiunto caso di test per la presa nel turno nero

--- src/test/java/it/uniba/interfaccia/TurnoNeroTest.java
+++ src/test/java/it/uniba/interfaccia/TurnoNeroTest.java
@@ -19,7 +19,8 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TurnoNeroTest {
-    Turno test = new TurnoNero();
+    Turno test = new TurnoBianco();
+    Turno testn = new TurnoNero();
     private final ByteArrayOutputStream out = new ByteArrayOutputStream();
     private final PrintStream originalOut = System.out;
 
@@ -34,18 +35,48 @@ class TurnoNeroTest {
     }
 
     @Test()
-    public void turnoGiocNeroSpostamentoTest() {
+    public void turnoGiocNeroSpostePresaTest() {
         Partita partita = new Partita();
-        Giocatore g = new Giocatore();
-        Comando cmd = new Comando();
-        //Mossa mossa = new MossaNero(0, 0);
-
-        g.setColore("nero");
-        String input = "9-13";
-        //String command = cmd.gestisciRegex(mossa,input);
+        String input = "21-18";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
         test.turnoGiocatore(partita,53);
+        String inpu = "9-13";
+        InputStream inp = new ByteArrayInputStream(inpu.getBytes());
+        System.setIn(inp);
+        testn.turnoGiocatore(partita,66);
+        String presa = "22-19";
+        InputStream pres = new ByteArrayInputStream(presa.getBytes());
+        System.setIn(pres);
+        test.turnoGiocatore(partita,66);
+        String spos1 = "10-14";
+        InputStream sposner = new ByteArrayInputStream(spos1.getBytes());
+        System.setIn(sposner);
+        testn.turnoGiocatore(partita,66);
+        String spos2 = "23-20";
+        InputStream sposbianc = new ByteArrayInputStream(spos2.getBytes());
+        System.setIn(sposbianc);
+        test.turnoGiocatore(partita,80);
+        String spos3 = "14x23";
+        InputStream sposner1 = new ByteArrayInputStream(spos3.getBytes());
+        System.setIn(sposner1);
+        testn.turnoGiocatore(partita,66);
+        String spos4 = "28x19";
+        InputStream sposbianc1 = new ByteArrayInputStream(spos4.getBytes());
+        System.setIn(sposbianc1);
+        test.turnoGiocatore(partita,80);
+        String spos5 = "11-15";
+        InputStream sposbianc2 = new ByteArrayInputStream(spos5.getBytes());
+        System.setIn(sposbianc2);
+        testn.turnoGiocatore(partita,66);
+        String presamult = "31-28";
+        InputStream presam = new ByteArrayInputStream(presamult.getBytes());
+        System.setIn(presam);
+        test.turnoGiocatore(partita,80);
+        String presa5 = "13x22x31";
+        InputStream pres2 = new ByteArrayInputStream(presa5.getBytes());
+        System.setIn(pres2);
+        testn.turnoGiocatore(partita,1231);
 
     }
 
